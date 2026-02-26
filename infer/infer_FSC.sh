54e7a4e7095fa99d8f981425324f3f4b04643fac
HaSon47 <153425469+HaSon47@users.noreply.github.com>
2026-02-26 21:11:05 +0700

    Update inference script to use ppt-v1 version

--- infer/infer_FSC.sh
+++ infer/infer_FSC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES='1' python infer_FSC.py \
---version ppt-v2 \
---checkpoint_dir /mnt/disk2/hachi/checkpoints/ppt-v2 \
---output_path /mnt/disk2/hachi/Output/test_to_define/FSC_final/FSC_easy_overlap/ctxt-guided_ppt2 \
+--version ppt-v1 \
+--checkpoint_dir /mnt/disk2/hachi/checkpoints/ppt-v1 \
+--output_path /mnt/disk2/hachi/Output/test_to_define/FSC_final/FSC_easy_overlap/ctxt-guided_ppt1 \
 --data_folder_path /mnt/disk2/hachi/data/FSC_final/FSC_easy_overlap \
 --task ctxt-guided
